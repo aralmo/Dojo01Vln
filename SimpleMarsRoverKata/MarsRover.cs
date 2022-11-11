3b9282b4be123675e42bc655d40259d76f5bb5b8
Aridane Alamo <aralmo@gmail.com>
2022-11-11 18:01:20 +0100

    Implement basic directions

---
+++ SimpleMarsRoverKata/MarsRover.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleMarsRoverKata
+{
+    public static class MarsRover
+    {
+        const string DIRECTIONSL = "NWSE";
+        const string DIRECTIONSR = "NESW";
+        public static string Execute(string command)
+        {
+            var lcount = command.Count(c => c == 'L');
+            var rcount = command.Count(c => c == 'R');
+
+            if (lcount > 0)
+                return $"0:0:{DIRECTIONSL[lcount % 4]}";
+            else
+                return $"0:0:{DIRECTIONSR[rcount % 4]}";
+        }
+    }
+}
---
+++ SimpleMarsRoverKata/MarsRoverShould.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+
+namespace SimpleMarsRoverKata
+{
+    public class MarsRoverShould
+    {
+        [Fact]
+        public void BeOnStartingPosition()
+        {
+            //According to specs the rover should start in 0:0:N position.
+
+            MarsRover
+                .Execute("")
+                .Should()
+                .Be("0:0:N");
+        }
+
+        [Theory]
+        [InlineData("L", "0:0:W")]
+        [InlineData("LL", "0:0:S")]
+        [InlineData("LLL", "0:0:E")]
+        [InlineData("LLLL", "0:0:N")]
+        [InlineData("LLLLL", "0:0:W")]
+        public void TurnLeft(string command, string expected)
+        {
+            MarsRover
+                .Execute(command)
+                .Should()
+                .Be(expected);
+        }
+
+        [Theory]
+        [InlineData("R", "0:0:E")]
+        [InlineData("RR", "0:0:S")]
+        [InlineData("RRR", "0:0:W")]
+        [InlineData("RRRR", "0:0:N")]
+        [InlineData("RRRRR", "0:0:E")]
+        public void TurnRight(string command, string expected)
+        {
+            MarsRover
+                .Execute(command)
+                .Should()
+                .Be(expected);
+        }
+
+        [Theory]
+        [InlineData("LR", "0:0:N")]
+        public void BeAbleToTurnLeftAndRight(string command, string expected)
+        {
+            MarsRover
+                .Execute(command)
+                .Should()
+                .Be(expected);
+        }
+
+    }
+}
